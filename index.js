9fe851e3bc5f18236475923116c2e650b749f5b3
Kimbangul <highcolor_12@naver.com>
2023-05-02 15:00:13 +0900

    feat: sample data add

--- index.js
+++ index.js
@@ -2,14 +2,37 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+// PARAM 상수
+const URL = "https://velog.io/@kimbangul";
+
 const getHtml = async () => {
   try {
-    const html = await axios.get('');
-    let articleList = [];
+    const html = await axios.get(URL);
+    const $ = cheerio.load(html.data);
+
+    let content = [];
+    const articleList = $("li.PostsList__Item");
+    
+    articleList.map((el, idx) => {
+      console.log(el, idx);
+      content[idx] = {
+        head: $(el).find("h3.PostsList__Text").text().replace(/\s/g, ""),
+        date: 'date',
+        context: 'context',
+        href: 'href',
+        headline: 'headline',
+        tags: 'tags',
+      }
+    });
+
+    console.log(content);
+    return content;
   }
   catch(e){
     console.log(e)
   }
 }
 
-getHtml();
+const articles = getHtml();
+
+console.log(articles);
