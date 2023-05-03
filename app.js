3a96ba1de293a96489ff9571ee05d00035ccee08
Kimbangul <highcolor_12@naver.com>
2023-05-03 09:19:35 +0900

    chore: package add(dotenv, cors)

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-import express from 'express';
-import crawler from './crawler';
+// const express = require('express');
+import Express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
-const app = express();
+import crawler from './crawler/index.js';
+import router from './routes/index.js';
+// const dotenv = require('dotenv');
+// const cors = require('cors');
+// const crawler = require('./crawler/index');
+// const router = require('./routes/index');
+
+dotenv.config();
+
+// NOTE config
+const app = Express();
+const port = process.env.PORT;
+
+// NOTE 접속 설정
+app.use(cors({
+  origin: '*', // TODO
+}));
+
+// NOTE router
+app.use('/', router);
 
 app.use(
   express.json({
